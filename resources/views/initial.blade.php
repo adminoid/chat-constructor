9afe1c47119a2bf0574df6096cb7f86956d5ca11
Petr Zenin <mr@adminoid.com>
2019-01-23 16:28:42 +0300

    installed main template, js and css stuff

---
+++ resources/views/initial.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>This is private panel for customers</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+</head>
+<body>
+
+<div class="container">
+    <div id="app">
+        <private-root-component></private-root-component>
+    </div>
+</div>
+
+<script src="{{ asset('/js/private.js') }}"></script>
+
+</body>
+</html>
