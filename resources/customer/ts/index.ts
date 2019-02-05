57cacfda7ba97d4dd51bc80fdeefa41c28ce7ce4
Petr Zenin <mr@adminoid.com>
2019-02-05 18:05:20 +0300

    typescript, sass, ts works fine; moved resources folder

---
+++ resources/customer/ts/index.ts
@@ -0,0 +1,11 @@
+import "./bootstrap"
+
+class App {
+
+  public var1: string = 'test'
+
+}
+
+let app = new App();
+
+console.log(app.var1);
---
+++ resources/customer/views/index.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>This is private panel for customers</title>
+    <link rel="stylesheet" href="{{ asset('/css/customer.css') }}">
+</head>
+<body>
+
+
+<div class="row no-gutters" id="app">
+
+    <private-root-sidebar></private-root-sidebar>
+    <private-root-work-area></private-root-work-area>
+
+</div>
+
+
+<footer>
+    <div class="copy">
+        &copy; 2019
+    </div>
+</footer>
+
+<script src="{{ asset('/js/customer.js') }}"></script>
+
+</body>
+</html>
